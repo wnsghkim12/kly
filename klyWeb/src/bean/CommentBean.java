959900344a67db8b86b2aad175fdb3cdb9015f10
danny <gryb809@gmail.com>
2018-07-25 16:07:04 +0900

    0.9.2 댓글 및 좋아요(추천) 테이블  및 관련 DTO 생성, Dummy 데이터 입력

--- klyWeb/src/bean/CommentBean.java
+++ klyWeb/src/bean/CommentBean.java
@@ -1,5 +1,52 @@
 package bean;
 
-public class CommentBean {
+import java.util.Date;
 
+public class CommentBean {
+	private int BOARD_NUM;
+	private String MEMBER_ID;
+	private int COMMENT_NUM;
+	private String COMMENT_CON;
+	private Date COMMENT_DATE;
+	private int COMMENT_BLIND;
+	public int getBOARD_NUM() {
+		return BOARD_NUM;
+	}
+	public void setBOARD_NUM(int bOARD_NUM) {
+		BOARD_NUM = bOARD_NUM;
+	}
+	public String getMEMBER_ID() {
+		return MEMBER_ID;
+	}
+	public void setMEMBER_ID(String mEMBER_ID) {
+		MEMBER_ID = mEMBER_ID;
+	}
+	public int getCOMMENT_NUM() {
+		return COMMENT_NUM;
+	}
+	public void setCOMMENT_NUM(int cOMMENT_NUM) {
+		COMMENT_NUM = cOMMENT_NUM;
+	}
+	public String getCOMMENT_CON() {
+		return COMMENT_CON;
+	}
+	public void setCOMMENT_CON(String cOMMENT_CON) {
+		COMMENT_CON = cOMMENT_CON;
+	}
+	public Date getCOMMENT_DATE() {
+		return COMMENT_DATE;
+	}
+	public void setCOMMENT_DATE(Date cOMMENT_DATE) {
+		COMMENT_DATE = cOMMENT_DATE;
+	}
+	public int getCOMMENT_BLIND() {
+		return COMMENT_BLIND;
+	}
+	public void setCOMMENT_BLIND(int cOMMENT_BLIND) {
+		COMMENT_BLIND = cOMMENT_BLIND;
+	}
+	
+	
+	
+	
 }
---
+++ klyWeb/src/bean/LikeBean.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class LikeBean {
+	private int LIKE_NUM;
+	private int BOARD_NUM;
+	private String MEMBER_ID;
+	public int getLIKE_NUM() {
+		return LIKE_NUM;
+	}
+	public void setLIKE_NUM(int lIKE_NUM) {
+		LIKE_NUM = lIKE_NUM;
+	}
+	public int getBOARD_NUM() {
+		return BOARD_NUM;
+	}
+	public void setBOARD_NUM(int bOARD_NUM) {
+		BOARD_NUM = bOARD_NUM;
+	}
+	public String getMEMBER_ID() {
+		return MEMBER_ID;
+	}
+	public void setMEMBER_ID(String mEMBER_ID) {
+		MEMBER_ID = mEMBER_ID;
+	}
+	
+	
+}
